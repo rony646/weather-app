1df3e73465cf208e998507f6549b0d80ba1d8a1d
rony646 <rony646@users.noreply.github.com>
2020-12-19 17:06:14 -0300

    inital commit

---
+++ src/Components/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MainPage.css';
+
+const MainPage = props => {
+    return(
+        <div className="main-page">
+            <p>Here goes the quote</p>
+            <p>weather resume</p>
+            <p>Current temperature</p>
+            <p>Location</p>
+        </div>
+    );
+};
+
+export default MainPage;
